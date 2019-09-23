5eedca0d5410a9b0ef169e75c6439b3ab766979f
Taylor Price <trprice@gmail.com>
2019-09-23 14:47:48 -0700

    Initial commit

---
+++ EveDataRetriever.h
@@ -0,0 +1,10 @@
+﻿// EveDataRetriever.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+// TODO: Reference additional headers your program requires here.
